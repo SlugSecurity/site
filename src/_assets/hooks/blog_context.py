9493dc37acc2405a08328f271d6b0cc994a2e38b
daniel <106453412+0xfbad@users.noreply.github.com>
2024-09-08 22:50:09 -0400

    Update comments for clarity

--- src/_assets/hooks/blog_context.py
+++ src/_assets/hooks/blog_context.py
@@ -1,9 +1,11 @@
 '''
-Adds posts and writeups to the context a page
+Adds posts and writeups data for anypage with "blog_content" in the front matter
+For example, its used on the homepage to source the two columns showing the most recent posts and writeups
 '''
 
 def on_page_context(context, page, config, nav):
-	if not page.meta.get("blog_context"): return
+	if not page.meta.get("blog_context"):
+		return
 
 	context["posts"] = config["plugins"]["material/blog"].blog.posts
 	context["writeups"] = config["plugins"]["material/blog #2"].blog.posts
